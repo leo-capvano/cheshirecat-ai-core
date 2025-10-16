eedb3ea6d51b1de63b926a38fe1cc538dbddeac3
pieroit <piero.savastano@gmail.com>
2025-10-16 19:50:54 +0200

    reinstantiate mcp client on vonfig changes

--- src/cat/protocols/model_context/client.py
+++ src/cat/protocols/model_context/client.py
@@ -1,8 +1,12 @@
+from typing import Dict
 from cachetools import TTLCache
 from slugify import slugify
-from fastmcp.client import Client
+from fastmcp import FastMCP, Client
 
 from cat.log import log
+
+# necessary in case of empty client config
+empty_server = FastMCP("EmptyServer")
 
 
 class MCPClient(Client):
@@ -10,28 +14,12 @@ class MCPClient(Client):
         We use caches waiting for the protocol to become stateless.
     """
 
-    def __init__(self, cat):
+    def __init__(self, config):
 
         # TODO: get addresses / tokens / api keys from DB
-        config = {
-            "mcpServers": {}
-        }
-
-        for slug, server_config in cat._ccat.mcps:
-            config["mcpServers"][slug] = {
-                "url": str(server_config.url)
-            }
-
-        for url in cat.chat_request.context.mcps:
-            slug = slugify(
-                str(url).split("://")[1],
-                separator="_",
-                
-            )
-            config["mcpServers"][slug] = {
-                "url": str(url)
-            }
-
+        self.config = config
+        if len(config["mcpServers"]) == 0:
+            super().__init__(empty_server)
         super().__init__(config)
 
 
@@ -42,8 +30,42 @@ class MCPClients():
         self.clients = TTLCache(maxsize=1000, ttl=60*10)
     
     def get_user_client(self, cat):
-        # TODOV2: check also server config did not change
-        if cat.user_id not in self.clients:
-            self.clients[cat.user_id] = MCPClient(cat) # open context and save it in the cache
+        
+        need_new, config = self.need_new_client(cat)
+        if need_new:
+            self.clients[cat.user_id] = MCPClient(config)
+
         return self.clients[cat.user_id]
+    
+    def need_new_client(self, cat) -> tuple[bool, dict]:
+        
+        config = {
+            "mcpServers": {}
+        }
+
+        for slug, server_config in cat._ccat.mcps:
+            config["mcpServers"][slug] = {
+                "url": str(server_config.url)
+            }
+        for url in cat.chat_request.context.mcps:
+            slug = slugify(
+                str(url).split("://")[1],
+                separator="_",          
+            )
+            config["mcpServers"][slug] = {
+                "url": str(url)
+            }
+        
+        need_new = (cat.user_id not in self.clients) \
+            or self.clients[cat.user_id].config != config
+
+        return (
+            need_new,
+            config
+        )
+        
+
+    
+        
+
     
