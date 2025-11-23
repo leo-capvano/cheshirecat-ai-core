56141cd75a5a97c3a775ea5a21f3f8750d0a7910
pieroit <piero.savastano@gmail.com>
2025-11-23 11:05:03 +0100

    simplify endpoints refresh

--- src/cat/looking_glass/cheshire_cat.py
+++ src/cat/looking_glass/cheshire_cat.py
@@ -37,10 +37,6 @@ class CheshireCat:
             self.fastapi_app = fastapi_app
             # reference to the cat in fastapi state
             fastapi_app.state.ccat = self
-            self.core_routes_sign = []
-            for r in fastapi_app.routes:
-                signature = self.get_route_signature(r)
-                self.core_routes_sign.append(signature)
 
             # init Factory
             self.factory = Factory()
@@ -88,43 +84,20 @@ class CheshireCat:
     def refresh_endpoints(self):
         """Sync plugin endpoints with the fastapi app."""
 
-        log.info(self.core_routes_sign)
-
-        # create a signature for every custom endpoint route
-        custom_routes_sign = []
-        for e in self.mad_hatter.endpoints:
-            for r in e.routes:
-                signature = self.get_route_signature(r)
-                custom_routes_sign.append(signature)
-                log.info(signature)
-
         # remove all CatEndpoint routes from fastapi app
         routes_to_remove = []
         for route in self.fastapi_app.routes:
-            signature = self.get_route_signature(route)
-            if signature not in self.core_routes_sign:
-                log.critical(route.plugin_id)
+            if hasattr(route.endpoint, 'plugin_id'):
                 routes_to_remove.append(route)
-
         for route in routes_to_remove:
-            log.error(signature)
             self.fastapi_app.routes.remove(route)
-            if route in self.fastapi_app.router.routes:
-                self.fastapi_app.router.routes.remove(route)
-            self.fastapi_app.openapi_schema = None  # reset openapi schema
         
         # add the new list
         for e in self.mad_hatter.endpoints:
             self.fastapi_app.include_router(e)
-            self.fastapi_app.openapi_schema = None  # reset openapi schema
-
-
-    def get_route_signature(self, route):
-        try:
-            signature = f"{route.name}-{route.path}-{route.tags}-{route.methods}"
-        except:
-            signature = f"{route.name}-{route.path}"
-        return signature
+        
+        # reset openapi schema
+        self.fastapi_app.openapi_schema = None
 
     @property
     def plugin(self):
