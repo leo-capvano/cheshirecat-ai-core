cf3bda03af55cd9fc2df3fd4d8f0b678153c61e0
pieroit <piero.savastano@gmail.com>
2026-03-11 22:13:43 +0100

    remove legacy plugins

--- tmp_core_plugins/rabbit_hole/hooks.py
+++
@@ -1,7 +0,0 @@
-from cat.mad_hatter.decorators import hook
-from .rabbit_hole import RabbitHole
-
-@hook
-def after_cat_bootstrap(caller):
-    # Rabbit Hole Instance
-    caller.rabbit_hole = RabbitHole(caller)  # TODOV2: check if it works, I guess not yet
